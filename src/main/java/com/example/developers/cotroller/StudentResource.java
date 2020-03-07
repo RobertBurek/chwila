4b9032b40a33e700f42f0f93bfa6374944bd713d
Robert <you@example.com>
2020-03-07 15:16:26 +0100

    Endpoint do zmiennej w properties

--- src/main/java/com/example/developers/cotroller/StudentResource.java
+++ src/main/java/com/example/developers/cotroller/StudentResource.java
@@ -20,9 +20,9 @@ import java.util.Optional;
 
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("${path.App}")
 @Transactional
-public class StudentResource {
+public class  StudentResource {
 
     private final Logger log = LoggerFactory.getLogger(StudentResource.class);
 
